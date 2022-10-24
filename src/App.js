5ee43d2758d0246158a2531b545aca2b217c11ca
Omnitole <tkkirienko@gmail.com>
2022-10-24 19:30:26 +0300

    hw4

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import {useForm} from "react-hook-form";
 
 function App() {
+
+  let {register,handleSubmit} = useForm()
+  let submit = (obj) => {
+    fetch('https://jsonplaceholder.typicode.com/users', {
+      method: 'Post',
+      body: JSON.stringify(obj),
+      headers: {'Content-type': 'application/json; charset=UTF-8'}
+    })
+        .then(response => response.json())
+        .then(result => console.log(result))
+  }
+
   return (
     <div>
+      <form onSubmit={handleSubmit(submit)}>
+        <input type="text" {...register("name")}/>
+        <input type="text"{...register("username")}/>
+        <button> Submit user </button>
+
+      </form>
 
     </div>
   );
